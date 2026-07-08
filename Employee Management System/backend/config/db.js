ccd5a33ff7903347a242fde3099f8273ab05bd7e
Waleeja Ali <waleeja.ali25@gmail.com>
2026-07-08 18:49:34 +0500

    update db string

--- Employee Management System/backend/config/db.js
+++ Employee Management System/backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/backend';
+  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/employee-management-system';
   await mongoose.connect(uri);
   console.log('Connected to MongoDB');
 }
